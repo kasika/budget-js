af36f713d966d99a86c6feb8290c4c9c6fdea0e6
Kasia Kowalska <kovalskyyyyy@gmail.com>
2020-08-27 22:09:32 -0300

    Set up UI and modules

---
+++ app.js
@@ -0,0 +1,42 @@
+
+// Budget controller
+var budgetController = (function() {
+
+})();
+
+
+
+// UI controller
+var UIController = (function() {
+
+})();
+
+
+
+// Global app controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+  var ctrlAddItem = function() {
+
+    // 1. Get the filed input data
+
+    // 2. Add the item to te budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+
+  });
+
+})(budgetController, UIController);
