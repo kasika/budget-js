a500dcfa19e449458b452acc8990581885a791e2
Kasia Kowalska <kovalskyyyyy@gmail.com>
2020-09-06 23:30:46 -0300

    Add init function

--- app.js
+++ app.js
@@ -37,7 +37,19 @@ var UIController = (function() {
 // Global app controller
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventisteners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+
   var ctrlAddItem = function() {
 
     // 1. Get the filed input data
@@ -52,18 +64,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log('App started');
+      setupEventisteners();
     }
-
-
-
-  });
+  };
 
 })(budgetController, UIController);
+
+controller.init();
